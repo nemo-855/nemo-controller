0552265030409a5b2fcead91f36f5b380bbead01
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-10-26 01:43:06 +0900

    [improvement] define MainViewModel

---
+++ app/src/main/java/com/nemo/macroid/ui/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.nemo.macroid.ui
+
+import androidx.lifecycle.ViewModel
+import com.nemo.macroid.data.repository.ControllerRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val controllerRepository: ControllerRepository
+) : ViewModel()
